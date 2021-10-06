369b74fda4898434e3f374f76c66565f233b9275
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-10-06 11:27:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
+provider "google" {
+  access_token = var.access_token
+}
+
 resource "google_secret_manager_secret" "secret-basic" {
-  secret_id = "secret"
+  secret_id = "secret-wf-google"
 
   labels = {
     label = "my-label"
